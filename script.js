65ce892bb89e5d6facab1789d0a2ca1b50a0c8e7
Ondřej Honus <honusondrej@gmail.com>
2023-12-30 12:15:35 +0100

    03 - eh

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("email").innerText="ondrasihonda@gmail.com";    
 });
+function emailCopy() {
+        navigator.clipboard.writeText("honusondrej@gmail.com");
+    }
+    function outFunc() {
+        var tooltip = document.getElementById("myTooltip");
+        tooltip.innerHTML = "Copy to clipboard";
+      }
