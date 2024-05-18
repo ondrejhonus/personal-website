49f44c7c853a1065023d52e78623fd369bb97354
ondrejhonus <honusondrej@gmail.com>
2024-05-18 17:10:46 +0200

    09 - dark mode switch, scrollbar, final touches

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const switchMode = document.getElementById('lightThing');
+const html = document.getElementById('themeSwitch');
+const switchIcon = document.getElementById('switchIcon');
+let isDark = false;
+
+function darkModeSwitch(){
+    if(isDark){
+        html.removeAttribute('data-theme');
+        html.setAttribute('data-theme', 'light');
+        switchIcon.classList.add('fas', 'fa-sun');
+        switchIcon.classList.remove('fa-moon');
+        isDark = false;
+    }
+    else{
+        html.removeAttribute('data-theme');
+        html.setAttribute('data-theme', 'dark');
+        switchIcon.classList.add('fas', 'fa-moon');
+        switchIcon.classList.remove('fa-sun');
+        isDark = true;
+    }
+}
+darkModeSwitch();
+
+switchIcon.addEventListener('click', ()=>{
+    darkModeSwitch();
+});
 
